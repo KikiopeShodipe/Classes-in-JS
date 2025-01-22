afa2339014c82a68d662b82e15b3bb936d21a11f
KikiopeShodipe <gilliansifuma@gmail.com>
2025-01-22 18:52:52 +0100

    Inneritance

---
+++ script.js
@@ -0,0 +1,19 @@
+class Car{
+    constructor(brand){
+        this.carname = brand;
+    }
+    present(){
+        return 'I have a ' + this.carname;
+    }
+}
+class Model extends Car{ // innheritence
+    constructor(brand,mod){
+        super(brand);
+        this.model = mod;
+    }
+    show(){
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+let mycar = new Model('Ford','Mustang');
+document.getElementById('details').innerHTML = mycar.show();
